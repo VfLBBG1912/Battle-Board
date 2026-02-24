fb6dcc49f2e99b936ecc28d87d7e24e240019d3f
VfLBBG1912 <steffen.fuehring@gmail.com>
2026-02-24 17:45:21 +0100

    Create hof.js

---
+++ hof.js
@@ -0,0 +1,32 @@
+{
+  "updatedAt": "2026-02-24",
+  "players": [
+    { "name": "David", "team": "Alpha Team", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Daniel", "team": "Alpha Team", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Leon", "team": "Alpha Team", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Noel", "team": "Alpha Team", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Imran", "team": "Alpha Team", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Sören", "team": "Alpha Team", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Jan", "team": "Alpha Team", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Bennet", "team": "Alpha Team", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Erich", "team": "Alpha Team", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Hauke", "team": "Alpha Team", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Lars", "team": "Alpha Team", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+
+    { "name": "Steffen", "team": "Team Bravo", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Max", "team": "Team Bravo", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Rille", "team": "Team Bravo", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Tim", "team": "Team Bravo", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Pascal", "team": "Team Bravo", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Joey", "team": "Team Bravo", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Justin", "team": "Team Bravo", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Kilian", "team": "Team Bravo", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Klemens", "team": "Team Bravo", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Maurice", "team": "Team Bravo", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Ferhat", "team": "Team Bravo", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+
+    { "name": "Bastian", "team": "Joker", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Eser", "team": "Joker", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 },
+    { "name": "Raoul", "team": "Joker", "maxLevel": 1, "maxChallenges": 0, "maxAllTimeSkillsTotal": 0 }
+  ]
+}
